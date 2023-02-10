7a84856c9b1e1d193227899bc02fab9b12cca784
taufiqalfianto <taufikalfianto8@students.amikom.ac.id>
2023-02-10 10:22:08 +0700

    response body

--- source/handler.js
+++ source/handler.js
@@ -61,7 +61,7 @@ const addBookHandler = (request, h) => {
     return response;
   }
   const response = h.response({
-    status: 'fail',
+    status: 'error',
     message: 'Buku gagal ditambahkan',
   });
   response.code(500);
